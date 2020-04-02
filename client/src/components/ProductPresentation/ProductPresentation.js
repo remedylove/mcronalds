3743c081c1fa62e94419eaa3c81892b7ad7fa0e5
Łukasz Pierzchała <lukaasz.pierzchala@gmail.com>
2020-04-03 00:19:59 +0200

    Refactored code in SingleProductView component - moved some lines of code to new component ProductPresentation

---
+++ client/src/components/ProductPresentation/ProductPresentation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Grid, Typography } from '@material-ui/core';
+import CurrencyFormat from 'react-currency-format';
+
+const ProductPresentation = ({ id, title, calories, description}) => {
+    return (
+        <Container style={{width: '80%'}}>
+            <Grid container style={{padding: '100px 0'}}>
+                <Grid item md={5}>
+                    <Typography variant="h3" align="left" style={{marginTop: '50px', fontWeight: 'bold'}}>{title}</Typography>
+                    <Typography variant="h4" align="left" style={{margin: '20px 0'}}>
+                        <CurrencyFormat value={calories} displayType={'text'} sufix={'kcal'}></CurrencyFormat>
+                        kcal
+                    </Typography>
+                    <Typography align="left" style={{marginTop: '25px'}}>{description}</Typography>
+                </Grid>
+                <Grid item md={7}align="left">
+                    <img src={require(`../../assets/${id}-details.jpg`)} alt="details-product-img" style={{maxWidth: '100%', marginTop: '25px'}}/>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default ProductPresentation
--- client/src/components/SingleProductView/SingleProductView.js
+++ client/src/components/SingleProductView/SingleProductView.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Typography, Container } from '@material-ui/core';
-import CurrencyFormat from 'react-currency-format';
+import ProductPresentation from '../ProductPresentation/ProductPresentation';
 import IngredientsList from '../IngredientsList/IngredientsList';
 
 class SingleProductView extends Component {
@@ -12,21 +11,7 @@ class SingleProductView extends Component {
         return (
             <div>
                 <React.Fragment>
-                    <Container style={{width: '80%'}}>
-                        <Grid container style={{padding: '100px 0'}}>
-                            <Grid item md={5}>
-                                <Typography variant="h3" align="left" style={{marginTop: '50px', fontWeight: 'bold'}}>{title}</Typography>
-                                <Typography variant="h4" align="left" style={{margin: '20px 0'}}>
-                                    <CurrencyFormat value={calories} displayType={'text'} sufix={'kcal'}></CurrencyFormat>
-                                    kcal
-                                </Typography>
-                                <Typography align="left" style={{marginTop: '25px'}}>{description}</Typography>
-                            </Grid>
-                            <Grid item md={7}align="left">
-                                <img src={require(`../../assets/${id}-details.jpg`)} alt="details-product-img" style={{maxWidth: '100%', marginTop: '25px'}}/>
-                            </Grid>
-                        </Grid>
-                    </Container>
+                    <ProductPresentation id={id} title={title} calories={calories} description={description}/>
                     <IngredientsList title={title} ingredients={ingredients}/>
                 </React.Fragment>
             </div>
